c91f42884ee385aee530ab9bcd3a8a83478be61c
KM <kristoffer.mandelbaum@googlemail.com>
2014-10-26 09:14:54 +0100

    Mail Versand bei Registrierung eingefügt

--- UserModule/src/java/app/helper/MailHelper.java
+++ UserModule/src/java/app/helper/MailHelper.java
@@ -1,53 +1,42 @@
 package app.helper;
 
-import java.util.Properties;
+import java.util.Date;
+ 
 import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
- 
-public class MailHelper {
 
+public class MailHelper {
+ 
     /**
-     *
-     * @param recipent
-     * @param content
+     * Utility method to send simple HTML email
+     * @param session
+     * @param toEmail
+     * @param subject
+     * @param body
      */
-    public static void MailHelper(String recipent,String content, String subject) {
-        
-		Properties props = new Properties();
-		props.put("mail.smtp.host", "smtp.gmail.com");
-		props.put("mail.smtp.socketFactory.port", "465");
-		props.put("mail.smtp.socketFactory.class",
-				"javax.net.ssl.SSLSocketFactory");
-		props.put("mail.smtp.auth", "true");
-		props.put("mail.smtp.port", "465");
- 
-		Session session = Session.getDefaultInstance(props,
-			new javax.mail.Authenticator() {
-				protected PasswordAuthentication getPasswordAuthentication() {
-					return new PasswordAuthentication("hawarcadestation@googlemail.com","hawarcade");
-				}
-			});
- 
-		try {
- 
-			Message message = new MimeMessage(session);
-			message.setFrom(new InternetAddress("hawarcadestation@googlemail.com"));
-			message.setRecipients(Message.RecipientType.TO,
-					InternetAddress.parse(recipent));
-			message.setSubject(subject);
-			message.setText(content);
- 
-			Transport.send(message);
- 
-			System.out.println("Done");
- 
-		} catch (MessagingException e) {
-			throw new RuntimeException(e);
-		}
-	}
-} 
+    public static void sendEmail(Session session, String toEmail, String subject, String body){
+        try
+        {
+          MimeMessage msg = new MimeMessage(session);
+          //set message headers
+          msg.addHeader("Content-type", "text/HTML; charset=UTF-8");
+          msg.addHeader("format", "flowed");
+          msg.addHeader("Content-Transfer-Encoding", "8bit");
+          msg.setFrom(new InternetAddress("hawarcadestation@googlemail.com", "HAWArcade"));
+          msg.setReplyTo(InternetAddress.parse("hawarcadestation@googlemail.com", false));
+          msg.setSubject(subject, "UTF-8");
+          msg.setText(body, "UTF-8");
+          msg.setSentDate(new Date()); 
+          msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail, false));
+          //System.out.println("Message is ready");
+          Transport.send(msg);  
+          //System.out.println("EMail Sent Successfully!!");
+        }
+        catch (Exception e) {
+          e.printStackTrace();
+        }
+    }
+}
--- UserModule/src/java/app/model/RegistrationModel.java
+++ UserModule/src/java/app/model/RegistrationModel.java
@@ -1,4 +1,3 @@
-// Trägt Bestellung in DB ein
 
 package app.model;
 
@@ -9,11 +8,15 @@ import app.helper.SQLHelper;
 import app.helper.MailHelper;
 import java.security.MessageDigest;
 import java.sql.ResultSet;
+import java.util.Properties;
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
 
 public class RegistrationModel {
     SQLHelper sql;
     MailHelper mhp;
-    String mail ;
+    String mail ,fromEmail , password, toEmail;
    
     
     public RegistrationModel(){
@@ -27,7 +30,24 @@ public class RegistrationModel {
             ResultSet rs = sql.execQuery("SELECT id FROM user WHERE mail='"+c.getMail()+"'");
             if( !rs.next() ){
                sql.execNonQuery("INSERT INTO `user` (mail,password,salt) VALUES ('"+c.getMail()+"', '"+c.getPassword()+"', '1234')");
-               MailHelper.MailHelper("hh-amburg1985@web.de", "message jo", "betreff jo");
+               //MailVersand TLS
+                    fromEmail = "hawarcadestation@googlemail.com"; 
+                    password = "hawarcade"; 
+                    toEmail = c.getMail();              
+                    //System.out.println("TLSEmail Start");
+                    Properties props = new Properties();
+                    props.put("mail.smtp.host", "smtp.gmail.com"); //SMTP Host
+                    props.put("mail.smtp.port", "587"); //TLS Port
+                    props.put("mail.smtp.auth", "true"); //enable authentication
+                    props.put("mail.smtp.starttls.enable", "true"); //enable STARTTLS
+                    Authenticator auth = new Authenticator() {                              
+                                @Override
+                                protected PasswordAuthentication getPasswordAuthentication() {
+                                return new PasswordAuthentication(fromEmail, password);
+                                }
+                            };
+                    Session session = Session.getInstance(props, auth);
+                    MailHelper.sendEmail(session, toEmail,"Ihre Registrierung bei HAWArcadeStation", "Sie haben sich bei der HAW Aracde Station registriert. Bitte klicken Sie auf folgenden Link um die Registrierung abzuschließen.");
             }
             else{
                 c.addError("mail", "E-mail-Adresse existiert bereits");
