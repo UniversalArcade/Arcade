baf35d77473254668ebc6027582b78cc424da296
KM <kristoffer.mandelbaum@googlemail.com>
2015-01-08 09:31:35 +0100

    Mail Bug fixed

--- UserModule/src/java/app/helper/MailHelper.java
+++ UserModule/src/java/app/helper/MailHelper.java
@@ -13,15 +13,19 @@ public class MailHelper {
     
 	public static void sendMail(String subject, String recipent, String content) {
 		Properties props = new Properties();
-		props.put("mail.smtp.auth", "true");
-		props.put("mail.smtp.starttls.enable", "true");
 		props.put("mail.smtp.host", "smtp.gmail.com");
-		props.put("mail.smtp.port", "587");
+		props.put("mail.smtp.socketFactory.port", "465");
+		props.put("mail.smtp.socketFactory.class",
+				"javax.net.ssl.SSLSocketFactory");
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.port", "465");
+ 
                 
 		Session session = Session.getInstance(props,
 			new javax.mail.Authenticator() {
+                                @Override
 				protected PasswordAuthentication getPasswordAuthentication() {
-					return new PasswordAuthentication("hawarcadestation","hawarcade");
+					return new PasswordAuthentication("hawarcadestation","Hawarcade22!");
 				}
 			});
  
